0ad9ed5db6c4fd7613c918cf0cbb39f7ae71eb31
Rahul Ravikumar <tikurahul@gmail.com>
2024-12-24 13:40:13 -0800

    Make it easier to watch for changes in the tools folder

--- src/main/tools/src/flags.ts
+++ src/main/tools/src/flags.ts
@@ -9,8 +9,9 @@ export const HIGHLIGHT_STYLES = '/assets/highlight/styles/dracula.css';
 export const HIGHLIGHT_JS = '/assets/highlight/highlight.pack.js';
 
 /** Paths */
-export const MANIFEST_PATH = '../../resources/blog/md/manifest.json';
-export const MARKDOWN_PATH = '../../resources/blog/md';
-export const HTML_PATH = '../../resources/blog/html';
-export const CORE_PATH = '../../resources/blog/assets/core';
+const RESOURCES = '../../../resources';
+export const MANIFEST_PATH = `${RESOURCES}/blog/md/manifest.json`;
+export const MARKDOWN_PATH = `${RESOURCES}/blog/md`;
+export const HTML_PATH = `${RESOURCES}/blog/html`;
+export const CORE_PATH = `${RESOURCES}/blog/assets/core`;
 export const TOOLS_OUT_PATH = '../../tools/out';
