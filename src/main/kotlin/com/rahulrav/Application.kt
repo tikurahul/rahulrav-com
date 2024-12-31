74336b0e0e80414f0b247ffa34ebbd538e917117
Rahul Ravikumar <tikurahul@gmail.com>
2024-12-30 16:19:42 -0800

    More cleanup.
    * Remove usage of `gson`.
    * Fix `talks.md` to make the page responsive.
    * Configure `logback` correctly.

--- src/main/kotlin/com/rahulrav/Application.kt
+++ src/main/kotlin/com/rahulrav/Application.kt
@@ -3,13 +3,13 @@ package com.rahulrav
 import com.google.appengine.api.memcache.ErrorHandlers
 import com.google.appengine.api.memcache.MemcacheService
 import com.google.appengine.api.memcache.MemcacheServiceFactory
-import com.google.gson.Gson
 import io.ktor.http.*
 import io.ktor.http.content.*
 import io.ktor.serialization.gson.*
 import io.ktor.server.application.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.compression.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.plugins.cors.routing.*
@@ -17,19 +17,18 @@ import io.ktor.server.plugins.statuspages.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import org.slf4j.LoggerFactory
+import org.slf4j.event.Level
 import java.time.Duration
-import java.util.logging.Level
 
 object AppContext {
     private const val NAME = "rahulrav.com"
-    val gson = lazy { Gson() }
     val logger = lazy {
         LoggerFactory.getLogger(NAME)
     }
 
     val cache: Lazy<MemcacheService> = lazy {
         val cache = MemcacheServiceFactory.getMemcacheService()
-        cache.errorHandler = ErrorHandlers.getConsistentLogAndContinue(Level.INFO)
+        cache.errorHandler = ErrorHandlers.getConsistentLogAndContinue(java.util.logging.Level.INFO)
         cache
     }
 }
@@ -39,6 +38,10 @@ fun Application.main() {
         gzip()
         deflate()
     }
+    install(CallLogging) {
+        level = Level.INFO
+    }
+
     install(StatusPages) {
         exception<Throwable> { call, cause ->
             call.application.log.error("Something bad happened", cause)
